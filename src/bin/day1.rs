185eb0831f92bcb4c22e60a5d35e7175c008b438
Oscar Mulder <oscar.mulder@codam.nl>
2022-12-01 14:22:16 +0100

    cleanup

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,6 +1,8 @@
 use std::path::Path;
 use anyhow::Result;
 
+const DAY: u32 = 1;
+
 fn solve(input: &Path, take: usize) -> Result<u32> {
     let mut res: Vec<u32> = std::fs::read_to_string(input)?
         .split("\n\n")
@@ -11,42 +13,46 @@ fn solve(input: &Path, take: usize) -> Result<u32> {
 }
 
 fn main() {
-    let day = 1;
-    let path = format!("./data/{}.input", day);
+    let path = format!("./data/{}.input", DAY);
     let input = Path::new(&path);
 
     let solve_first = solve(input, 1);
     match solve_first {
-        Ok(res) => println!("Day {}, first puzzle: {}", day, res),
+        Ok(res) => println!("Day {}, first puzzle: {}", DAY, res),
         Err(e) => println!("{:?}", e),
     }
 
     let solve_second = solve(input, 3);
     match solve_second {
-        Ok(res) => println!("Day {}, second puzzle: {}", day, res),
+        Ok(res) => println!("Day {}, second puzzle: {}", DAY, res),
         Err(e) => println!("{:?}", e),
     }
 }
 
 #[cfg(test)]
 mod test {
-    use std::path::Path;
-    use crate::solve;
+    use super::*;
 
     #[test]
     fn example_first() {
-        let input = Path::new("./data/1.example");
+        let path = format!("./data/{}.example", DAY);
+        let input = Path::new(&path);
+
         let result = 24000;
         let solve = solve(input, 1);
+
         assert!(solve.is_ok());
         assert_eq!(solve.unwrap(), result);
     }
 
     #[test]
     fn example_second() {
-        let input = Path::new("./data/1.example");
+        let path = format!("./data/{}.example", DAY);
+        let input = Path::new(&path);
+
         let result = 45000;
         let solve = solve(input, 3);
+        
         assert!(solve.is_ok());
         assert_eq!(solve.unwrap(), result);
     }
