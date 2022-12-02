3612796bb45d20f766383ca338205804788a4ad7
Oscar Mulder <oscar.mulder@codam.nl>
2022-12-02 06:23:51 +0100

    add day 2

---
+++ src/bin/day2.rs
@@ -0,0 +1,106 @@
+use std::path::Path;
+use anyhow::Result;
+
+const DAY: u32 = 2;
+
+fn scores_1(round: &str) -> u32 {
+    match round {
+        "A X" => 4,
+        "A Y" => 8,
+        "A Z" => 3,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 7,
+        "C Y" => 2,
+        "C Z" => 6,
+        _ => 0
+    }
+}
+
+fn scores_2(round: &str) -> u32 {
+    match round {
+        "A X" => 3,
+        "A Y" => 4,
+        "A Z" => 8,
+        "B X" => 1,
+        "B Y" => 5,
+        "B Z" => 9,
+        "C X" => 2,
+        "C Y" => 6,
+        "C Z" => 7,
+        _ => 0
+    }
+}
+
+fn solve(input: &str, scores: fn(&str) -> u32) -> Result<String> {
+    let res: u32 = input.split("\n")
+        .map(scores)
+        .sum();
+    Ok(res.to_string())
+} 
+
+fn solve_1(input: &str) -> Result<String> {
+    solve(input, scores_1)
+}
+
+fn solve_2(input: &str) -> Result<String> {
+    solve(input, scores_2)
+}
+
+fn input() -> String {
+    let path = format!("./data/{}.input", DAY);
+    let input_path = Path::new(&path);
+    let input = std::fs::read_to_string(input_path).unwrap();
+    input
+}
+
+fn main() {
+    let input = input();
+
+    let solve_first = solve_1(&input);
+    match solve_first {
+        Ok(res) => println!("Day {}, first puzzle: {}", DAY, res),
+        Err(e) => println!("{:?}", e),
+    }
+
+    let solve_second = solve_2(&input);
+    match solve_second {
+        Ok(res) => println!("Day {}, second puzzle: {}", DAY, res),
+        Err(e) => println!("{:?}", e),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    fn example_input() -> String {
+        let path = format!("./data/{}.example", DAY);
+        let input_path = Path::new(&path);
+        let input = std::fs::read_to_string(input_path).unwrap();
+        input
+    }
+
+    #[test]
+    fn example_first() {
+        let input = example_input();
+
+        let result = "15";
+        let solve = solve_1(&input);
+
+        assert!(solve.is_ok());
+        assert_eq!(solve.unwrap(), result);
+    }
+
+    #[test]
+    fn example_second() {
+        let input = example_input();
+
+        let result = "12";
+        let solve = solve_2(&input);
+
+        assert!(solve.is_ok());
+        assert_eq!(solve.unwrap(), result);
+    }
+}
